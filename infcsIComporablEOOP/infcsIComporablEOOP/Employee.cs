30b059e31e0bdafd852dbd97de81a3730b2f1195
hunkaracr1 <114934461+hunkaracar@users.noreply.github.com>
2022-11-01 19:47:45 +0300

    Add files via upload

---
+++ infcsIComporablEOOP/infcsIComporablEOOP/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infcsIComporablEOOP
+{
+    public class Employee : IComparable
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Salary { get; set; }
+
+        public int CompareTo(object? obj)
+        {
+            var other = (Employee)obj;
+
+            if (this.Salary < other.Salary)
+            {
+                return -1;  
+            }
+            else if (this.Salary.Equals(other.Salary))
+            {
+                return 1;   
+            }
+            else
+            {
+                return 0;
+            }
+
+
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Id,-5} {FullName,-20 } {Salary,-7}";
+        }
+    }
+}
---
+++ infcsIComporablEOOP/infcsIComporablEOOP/Program.cs
@@ -0,0 +1,54 @@
+﻿using infcsIComporablEOOP;
+
+//Nesneye karşılaştırlabilir olma özelliği kazandırma
+namespace infcsIComporablEOOP;
+
+
+class Program
+{
+    public static void Main()
+    {
+        var employees = new List<Employee>()
+        {
+            new Employee()
+            {
+                Id = 1,
+                FullName = "Hunkar Acar",
+                Salary = 35000
+            },
+            new Employee()
+            {
+                Id = 2,
+                FullName = "Omer kasapoglu",
+                Salary = 25000
+            },
+            new Employee()
+            {
+                Id = 3,
+                FullName = "Semih Eseroglu",
+                Salary = 27000
+            },
+            new Employee()
+            {
+                Id = 4,
+                FullName = "Arda Yılmaz",
+                Salary = 10000
+            },
+            new Employee()
+              
+            {
+                Id = 5,
+                FullName = "Semih Akça",
+                Salary = 17000
+            }
+        };
+
+        employees.Sort(); //bu method interfacesde kullandığımız IComporobleble dan geldi => comporobleto karşılasştıma işine yarar (cast işlemi yaptık) 
+
+        foreach (var emp in employees)
+        {
+            Console.WriteLine(emp);
+        }
+    }
+}
+
